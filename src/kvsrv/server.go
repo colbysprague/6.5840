7be5a9d212c2363a753194d9a3559e1a234e019b
Colby Sprague <colbyjsprague@gmail.com>
2026-04-04 10:54:45 -0400

    basic kv logic implementation

--- src/kvsrv/server.go
+++ src/kvsrv/server.go
@@ -14,30 +14,56 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
-
 type KVServer struct {
-	mu sync.Mutex
-
-	// Your definitions here.
+	mu    sync.Mutex
+	cache map[string]string
 }
-
 
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
-	// Your code here.
+	kv.mu.Lock()
+	kv.mu.Unlock()
+
+	reply.Value = kv.get(args.Key)
 }
 
+func (kv *KVServer) get(key string) string {
+	val, ok := kv.cache[key]
+	if ok {
+		return val
+	}
+
+	return ""
+}
+
+func (kv *KVServer) set(key string, val string) {
+	kv.cache[key] = val
+}
+
+// puts the value
 func (kv *KVServer) Put(args *PutAppendArgs, reply *PutAppendReply) {
-	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	key, val := args.Key, args.Value
+	kv.set(key, val)
 }
 
+// puts and returns
 func (kv *KVServer) Append(args *PutAppendArgs, reply *PutAppendReply) {
-	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	key, val := args.Key, args.Value
+
+	prev := kv.get(key)
+	reply.Value = prev
+
+	kv.set(key, prev+val)
 }
 
 func StartKVServer() *KVServer {
 	kv := new(KVServer)
-
-	// You may need initialization code here.
+	kv.cache = make(map[string]string)
 
 	return kv
 }
